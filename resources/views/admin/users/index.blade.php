a64f7ff56b8341d68254603301679f86bc391ecd
Alexandre Carignan <alexandre.carignan@gmail.com>
2020-12-21 15:17:35 -0500

    modification de la page admin/users/index.blade.php pour affichier, éditer et supprimer un usager.

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -10,13 +10,29 @@ Users management
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Liste des utilisateurs</div>
-
-                    <div class="card-body">
-                        @foreach ($users as user)
-                            {{ $user->name }} - {{ $user-email }}
+                    <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">'#'</th>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                            <th scope="row">{{ $user->id }}</th>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>
+                                <a href="{{ route('admin.users.edit' ,$user->id) }}"><button class="btn btn-primary">Éditer</button></a>
+                                <a href="{{ route('admin.users.destroy', $user->id) }}"><button class="btn btn-warning">Suprimer</button></a>
+                            </td>
+                            </tr>
                         @endforeach
+                    </tbody>
 
-                    </div>
                 </div>
             </div>
         </div>
