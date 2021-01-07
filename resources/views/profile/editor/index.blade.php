f33585bf33ccb268ef193a5e990bfd1f68a6bf7c
Alexandre Carignan <alexandre.carignan@gmail.com>
2021-01-07 07:58:56 -0500

    ajustement des boutons de ville dans /profile/editor/index.

--- resources/views/profile/editor/index.blade.php
+++ resources/views/profile/editor/index.blade.php
@@ -65,9 +65,9 @@
                             @foreach($cities->chunk(8) as $villes)
                                 <div class="row justify-content-center">
                                     <tbody>
-                                        <div class="justify-content-center">
+                                        <div class="justify-content-center p-2">
                                             @foreach($villes as $ville)
-                                            <button class="btn btn-link p-2"><a href="{{ route('ville.city.show', $ville->id) }}">{{ $ville->name }}</a></button>
+                                            <span><button class="btn btn-link p-2"><a href="{{ route('ville.city.show', $ville->id) }}">{{ $ville->name }}</a></button></span>
                                             @endforeach
                                         </div>
                                     </tbody>
